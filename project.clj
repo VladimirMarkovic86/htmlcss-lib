4425473cd302d8c8247ccf02e359842911eb370b
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:45:04 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,9 +1,12 @@
-(defproject org.vladimir/htmlcss-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.0"
   :description "Simple HTML and CSS library"
-  :url "http://gitlab:1610/VladimirMarkovic86/htmlcss-lib"
+  :url "http://github.com/VladimirMarkovic86/htmlcss-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "1.10.339"]
                  ]
+
+  :min-lein-version "2.0.0"
+  
   :source-paths ["src/cljs"])
 
