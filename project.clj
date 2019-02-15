e8a60db565fa807e94c12fe68918b16b6d9ba2a1
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-15 11:03:39 +0100

    Task 54: Fixed po and ocr

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.2"
+(defproject org.clojars.vladimirmarkovic86/htmlcss-lib "0.1.3"
   :description "Simple HTML and CSS library"
   :url "http://github.com/VladimirMarkovic86/htmlcss-lib"
   :license {:name "Eclipse Public License"
